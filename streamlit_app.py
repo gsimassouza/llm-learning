cadfbde99721215c2689d319a008a1aaf9d24dc1
Guilherme Simas <49327630+gsimassouza@users.noreply.github.com>
2024-12-29 22:26:34 -0300

    adding model selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import os
 from groq import Groq
 import requests
 import pandas as pd
+import streamlit as st
 
 GROQ_API_KEY = os.environ["GROQ_API_KEY"]
 
@@ -12,7 +13,32 @@ headers = {
     "Content-Type": "application/json"
 }
 
-response = requests.get(url, headers=headers)
+st.sidebar.write("Choose the model you want to talk to!")
 
-print(pd.DataFrame(response.json()['data']))
+st.session_state.model_names = [
+    "whisper-large-v3",
+    "whisper-large-v3-turbo",
+    "mixtral-8x7b-32768",
+    "llama3-groq-70b-8192-tool-use-preview",
+    "llama3-groq-8b-8192-tool-use-preview",
+    "llama-3.2-90b-vision-preview",
+    "llama-3.1-70b-versatile",
+    "gemma2-9b-it",
+    "llama-3.3-70b-specdec",
+    "llama-3.2-1b-preview",
+    "llama3-70b-8192",
+    "distil-whisper-large-v3-en",
+    "llama-3.2-3b-preview",
+    "llama3-8b-8192",
+    "llama-guard-3-8b",
+    "llama-3.3-70b-versatile",
+    "llama-3.1-8b-instant",
+    "llama-3.2-11b-vision-preview"
+]
+
+st.sidebar.selectbox(label="model", options=st.session_state.model_names)
+
+st.title("Bora conversar!")
+
+# To-do: create the chat itself
 
