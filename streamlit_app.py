ec9c061344734157e48e5c50fb13bfb06af35042
Guilherme Simas <49327630+gsimassouza@users.noreply.github.com>
2024-12-31 01:09:36 -0300

    adding assistant responses

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,22 @@ from groq import Groq
 import requests
 import pandas as pd
 import streamlit as st
+import aisuite as ai
+from dotenv import load_dotenv
 
-GROQ_API_KEY = os.environ["GROQ_API_KEY"]
+load_dotenv()
 
-url = "https://api.groq.com/openai/v1/models"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-headers = {
-    "Authorization": f"Bearer {GROQ_API_KEY}",
-    "Content-Type": "application/json"
-}
+if "client" not in st.session_state:
+    st.session_state.client = ai.Client()
 
 st.sidebar.write("Choose the model you want to chat with!")
 
 st.session_state.model_names = [
-    "whisper-large-v3",
-    "whisper-large-v3-turbo",
+    # "whisper-large-v3",
+    # "whisper-large-v3-turbo",
+    "llama-3.3-70b-versatile",
     "mixtral-8x7b-32768",
     "llama3-groq-70b-8192-tool-use-preview",
     "llama3-groq-8b-8192-tool-use-preview",
@@ -27,11 +28,10 @@ st.session_state.model_names = [
     "llama-3.3-70b-specdec",
     "llama-3.2-1b-preview",
     "llama3-70b-8192",
-    "distil-whisper-large-v3-en",
+    # "distil-whisper-large-v3-en",
     "llama-3.2-3b-preview",
     "llama3-8b-8192",
-    "llama-guard-3-8b",
-    "llama-3.3-70b-versatile",
+    # "llama-guard-3-8b",
     "llama-3.1-8b-instant",
     "llama-3.2-11b-vision-preview"
 ]
@@ -51,10 +51,23 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-
-if prompt := st.chat_input("Enter a message to the AI"):
+prompt = st.chat_input("Enter a message to the AI")
+if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
 
     # add user message to history
     st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # send message to Groq API
+    response = st.session_state.client.chat.completions.create(
+        model="groq:"+st.session_state.selected_model,
+        messages=st.session_state.messages,
+        temperature=0.0
+    )
+    response_content = response.choices[0].message.content
+    
+    with st.chat_message("assistant"):
+        st.markdown(response_content)
+    
+    st.session_state.messages.append({"role": "assistant", "content": response_content})
